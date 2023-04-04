da14353341dbea13667e7c15061a08c2d1e1338c
Adria Barreto <adriabarreto12@gmail.com>
2023-04-04 15:05:14 -0300

    Adiciona correção dos erros

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #Recebe as informações do usuário:
-nome = input("Digite seu nome: ")
+nome = str(input("Digite seu nome: "))
 idade = int(input("Qual a sua idade hoje? "))
 anoatual = int(input("Em que ano estamos? "))
 outroano = int(input("Digite outro ano: "))
 
 
 #Idade no futuro
-nascimento = int(anoatual-idade)
-futuro = outroano-nascimento
+idadefutura = int(outroano-(anoatual-idade))
 
-print(f'{nome}, no ano de {outroano} você terá {futuro} anos.')
+
+print(f'{nome}, no ano de {outroano} você terá {idadefutura} anos.')
